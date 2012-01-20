0be4884efd97813f0d77a83232425c4246dc421a
Jan Tezner <jan.tezner@gmail.com>
2012-01-20 13:33:40 +0000

    Czech translation of lexicon

---
+++ core/components/ditsnews/lexicon/cs/default.inc.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Czech Lexicon Entries for DitsNews
+ *
+ * @package ditsnews
+ * @subpackage lexicon
+ *
+ * @author modxcms.cz
+ * @updated 2012-01-20
+ */
+
+/* general */
+// $_lang['ditsnews'] = 'DitsNews';
+$_lang['ditsnews'] = 'DitsNews';
+// $_lang['ditsnews.desc'] = 'Newsletter manager for MODX';
+$_lang['ditsnews.desc'] = 'Newsletter systém';
+// $_lang['ditsnews.menu'] = 'Menu';
+$_lang['ditsnews.menu'] = 'Menu';
+// $_lang['ditsnews.search...'] = 'Search...';
+$_lang['ditsnews.search...'] = 'Vyhledávání...';
+
+/* newsletters */
+// $_lang['ditsnews.newsletters'] = 'Newsletters';
+$_lang['ditsnews.newsletters'] = 'Newslettery';
+// $_lang['ditsnews.newsletters.subject'] = 'Subject';
+$_lang['ditsnews.newsletters.subject'] = 'Předmět';
+// $_lang['ditsnews.newsletters.date'] = 'Date';
+$_lang['ditsnews.newsletters.date'] = 'Datum';
+// $_lang['ditsnews.newsletters.document'] = 'Document';
+$_lang['ditsnews.newsletters.document'] = 'Dokument';
+// $_lang['ditsnews.newsletters.total'] = 'Total';
+$_lang['ditsnews.newsletters.total'] = 'Celkem';
+// $_lang['ditsnews.newsletters.sent'] = 'Sent';
+$_lang['ditsnews.newsletters.sent'] = 'Odesláno';
+// $_lang['ditsnews.newsletters.new'] = 'New newsletter';
+$_lang['ditsnews.newsletters.new'] = 'Vytvořit newsletter';
+// $_lang['ditsnews.newsletters.groups'] = 'Groups';
+$_lang['ditsnews.newsletters.groups'] = 'Skupiny';
+// $_lang['ditsnews.newsletters.remove'] = 'Remove newsletter';
+$_lang['ditsnews.newsletters.remove'] = 'Odstranit newsletter';
+// $_lang['ditsnews.newsletters.remove.title'] = 'Remove newsletter?';
+$_lang['ditsnews.newsletters.remove.title'] = 'Odstranit newsletter?';
+// $_lang['ditsnews.newsletters.remove.confirm'] = 'Are you sure you want to remove this newsletter and all it\'s data?';
+$_lang['ditsnews.newsletters.remove.confirm'] = 'Opravdu chcete odstranit tento newsletter a všehna jeho data?';
+// $_lang['ditsnews.newsletters.saved'] = 'Newsletter saved (scheduled)';
+$_lang['ditsnews.newsletters.saved'] = 'Newsletter uložen (naplánován)';
+// $_lang['ditsnews.newsletters.err.save'] = 'Could not save/schedule newsletter';
+$_lang['ditsnews.newsletters.err.save'] = 'Nelze uložit/naplánovat newsletter';
+// $_lang['ditsnews.newsletters.err.nf'] = 'Could not open/find document';
+$_lang['ditsnews.newsletters.err.nf'] = 'Nelze otevřít/nalézt dokument';
+// $_lang['ditsnews.newsletters.err.remove'] = 'Could not remove newsletter';
+$_lang['ditsnews.newsletters.err.remove'] = 'Nelze odstranit newsletter';
+
+/* groups */
+// $_lang['ditsnews.groups'] = 'Groups';
+$_lang['ditsnews.groups'] = 'Skupiny';
+// $_lang['ditsnews.groups.name'] = 'Name';
+$_lang['ditsnews.groups.name'] = 'Název';
+// $_lang['ditsnews.groups.public'] = 'Public';
+$_lang['ditsnews.groups.public'] = 'Veřejný';
+// $_lang['ditsnews.groups.public.desc'] = 'Public (allow subscription through form)';
+$_lang['ditsnews.groups.public.desc'] = 'Veřejný (umožňuje přidávání skrze formulář)';
+// $_lang['ditsnews.groups.members'] = 'Members';
+$_lang['ditsnews.groups.members'] = 'Členové';
+// $_lang['ditsnews.groups.members'] = 'Members';
+$_lang['ditsnews.groups.members'] = 'Členové';
+// $_lang['ditsnews.groups.new'] = 'New group';
+$_lang['ditsnews.groups.new'] = 'Vytvořit skupinu';
+// $_lang['ditsnews.groups.edit'] = 'Edit group';
+$_lang['ditsnews.groups.edit'] = 'Upravit skupinu';
+// $_lang['ditsnews.groups.remove'] = 'Remove group';
+$_lang['ditsnews.groups.remove'] = 'Odstranit skupinu';
+// $_lang['ditsnews.groups.remove.title'] = 'Remove group?';
+$_lang['ditsnews.groups.remove.title'] = 'Odstranit skupinu?';
+// $_lang['ditsnews.groups.remove.confirm'] = 'Are you sure you want to remove this group? Subscribers won\'t be deleted';
+$_lang['ditsnews.groups.remove.confirm'] = 'Opravdu chcete odstranit tuto skupinu? Odběratelé nebudou smazáni';
+// $_lang['ditsnews.groups.update'] = 'Update group';
+$_lang['ditsnews.groups.update'] = 'Upravit skupinu';
+// $_lang['ditsnews.groups.saved'] = 'Group saved';
+$_lang['ditsnews.groups.saved'] = 'Skupina uložena';
+// $_lang['ditsnews.groups.err.nf'] = 'Group not found';
+$_lang['ditsnews.groups.err.nf'] = 'Skupina nenalezena';
+// $_lang['ditsnews.groups.err.save'] = 'Could not save group';
+$_lang['ditsnews.groups.err.save'] = 'Nelze uložit skupinu';
+
+/* subscribers */
+// $_lang['ditsnews.subscribers'] = 'Subscribers';
+$_lang['ditsnews.subscribers'] = 'Odběratelé';
+// $_lang['ditsnews.subscribers.firstname'] = 'First name';
+$_lang['ditsnews.subscribers.firstname'] = 'Jméno';
+// $_lang['ditsnews.subscribers.lastname'] = 'Last name';
+$_lang['ditsnews.subscribers.lastname'] = 'Příjmení';
+// $_lang['ditsnews.subscribers.company'] = 'Company';
+$_lang['ditsnews.subscribers.company'] = 'Společnost';
+// $_lang['ditsnews.subscribers.email'] = 'Email';
+$_lang['ditsnews.subscribers.email'] = 'E-mail';
+// $_lang['ditsnews.subscribers.signupdate'] = 'Signup date';
+$_lang['ditsnews.subscribers.signupdate'] = 'Datum přihlášení';
+// $_lang['ditsnews.subscribers.new'] = 'New subscriber';
+$_lang['ditsnews.subscribers.new'] = 'Vytvořit odběratele';
+// $_lang['ditsnews.subscribers.exportcsv'] = 'Export CSV';
+$_lang['ditsnews.subscribers.exportcsv'] = 'Exportovat CSV';
+// $_lang['ditsnews.subscribers.importcsv'] = 'Import CSV';
+$_lang['ditsnews.subscribers.importcsv'] = 'Importovat CSV';
+// $_lang['ditsnews.subscribers.importcsv.start'] = 'Start import';
+$_lang['ditsnews.subscribers.importcsv.start'] = 'Zahájit import';
+// $_lang['ditsnews.subscribers.importcsv.file'] = 'File';
+$_lang['ditsnews.subscribers.importcsv.file'] = 'Soubor';
+// $_lang['ditsnews.subscribers.importcsv.results'] = 'Results';
+$_lang['ditsnews.subscribers.importcsv.results'] = 'Výsledky';
+// $_lang['ditsnews.subscribers.importcsv.err.uploadfile'] = 'Please, upload a file';
+$_lang['ditsnews.subscribers.importcsv.err.uploadfile'] = 'Prosím nahrajte soubor';
+// $_lang['ditsnews.subscribers.importcsv.err.cantopenfile'] = 'Can\'t open file';
+$_lang['ditsnews.subscribers.importcsv.err.cantopenfile'] = 'Soubor nelze otevřít';
+// $_lang['ditsnews.subscribers.importcsv.err.firstrow'] = 'First row must contain column names (first column must be email)';
+$_lang['ditsnews.subscribers.importcsv.err.firstrow'] = 'První řádek musí obsahovat názvy sloupců (první sloupec musí být e-mail)';
+// $_lang['ditsnews.subscribers.importcsv.err.cantsaverow'] = 'Can\'t save row [[+rownum]]';
+$_lang['ditsnews.subscribers.importcsv.err.cantsaverow'] = 'Nelze uložit řádek [[+rownum]]';
+// $_lang['ditsnews.subscribers.importcsv.err.skippedrow'] = 'Skipped row [[+rownum]]';
+$_lang['ditsnews.subscribers.importcsv.err.skippedrow'] = 'Řádek [[+rownum]] přeskočen';
+// $_lang['ditsnews.subscribers.importcsv.msg.complete'] = 'Import complete. Imported [[+importCount]] records ([[+newCount]] new)';
+$_lang['ditsnews.subscribers.importcsv.msg.complete'] = 'Import dokončen. Naimportováno [[+importCount]] záznamů (z toho [[+newCount]] nových)';
+// $_lang['ditsnews.subscribers.confirm.subject'] = 'Confirm your newsletter subscription';
+$_lang['ditsnews.subscribers.confirm.subject'] = 'Potvrzení odběru newsletteru';
+// $_lang['ditsnews.subscribers.confirm.success'] = 'You are now subscribed to our newsletter.';
+$_lang['ditsnews.subscribers.confirm.success'] = 'Nyní jste přihlášen k našemu newsletteru.';
+// $_lang['ditsnews.subscribers.confirm.err'] = 'Subscriber / code combination incorrect.';
+$_lang['ditsnews.subscribers.confirm.err'] = 'Nesprávná kombinace odběratel / kód.';
+// $_lang['ditsnews.subscribers.signup.err.emailunique'] = 'Email address already in use';
+$_lang['ditsnews.subscribers.signup.err.emailunique'] = 'Tento e-mail je již přihlášen';
+// $_lang['ditsnews.subscribers.unsubscribe.success'] = 'You have been removed from our mailing list.';
+$_lang['ditsnews.subscribers.unsubscribe.success'] = 'Byl jste odhlášen z mailing listu.';
+// $_lang['ditsnews.subscribers.unsubscribe.err'] = 'Subscriber not found.';
+$_lang['ditsnews.subscribers.unsubscribe.err'] = 'Odběratel nenalezen.';
+// $_lang['ditsnews.subscribers.active'] = 'Active';
+$_lang['ditsnews.subscribers.active'] = 'Aktivní';
+// $_lang['ditsnews.subscribers.groups'] = 'Groups';
+$_lang['ditsnews.subscribers.groups'] = 'Skupiny';
+// $_lang['ditsnews.subscribers.remove'] = 'Remove subscriber';
+$_lang['ditsnews.subscribers.remove'] = 'Odstranit odběratele';
+// $_lang['ditsnews.subscribers.remove.title'] = 'Remove subscriber?';
+$_lang['ditsnews.subscribers.remove.title'] = 'Odstranit odběratele?';
+// $_lang['ditsnews.subscribers.remove.confirm'] = 'Are you sure you want to remove this subscriber?';
+$_lang['ditsnews.subscribers.remove.confirm'] = 'Opravdu chcete odstranit tohoto odběratele?';
+// $_lang['ditsnews.subscribers.update'] = 'Update subscriber';
+$_lang['ditsnews.subscribers.update'] = 'Upravit odběratele';
+// $_lang['ditsnews.subscribers.saved'] = 'Subscriber saved';
+$_lang['ditsnews.subscribers.saved'] = 'Odběratel uložen';
+// $_lang['ditsnews.subscribers.err.save'] = 'Error while saving subscriber';
+$_lang['ditsnews.subscribers.err.save'] = 'Chyba ukládání odběratele';
+// $_lang['ditsnews.subscribers.err.ae'] = 'A subscriber with the same email address already exists';
+$_lang['ditsnews.subscribers.err.ae'] = 'Odběratel s tímto e-mailem již existuje';
+
+/* settings */
+// $_lang['ditsnews.settings'] = 'Settings';
+$_lang['ditsnews.settings'] = 'Nastavení';
+// $_lang['ditsnews.settings.name'] = 'Name';
+$_lang['ditsnews.settings.name'] = 'Jméno';
+// $_lang['ditsnews.settings.email'] = 'Email';
+$_lang['ditsnews.settings.email'] = 'E-mail';
+// $_lang['ditsnews.settings.bounceemail'] = 'Bounce email address';
+$_lang['ditsnews.settings.bounceemail'] = 'Return-path';
+// $_lang['ditsnews.settings.confirmpage'] = 'Confirmation page';
+$_lang['ditsnews.settings.confirmpage'] = 'Stránka s potvrzením';
+// $_lang['ditsnews.settings.unsubscribepage'] = 'Unsubscribe page';
+$_lang['ditsnews.settings.unsubscribepage'] = 'Stránka pro odhlášení';
+// $_lang['ditsnews.settings.template'] = 'Template';
+$_lang['ditsnews.settings.template'] = 'Šablona';
+// $_lang['ditsnews.settings.saved'] = 'Settings saved';
+$_lang['ditsnews.settings.saved'] = 'Nastavení uloženo';
+// $_lang['ditsnews.settings.error'] = 'Error while saving settings';
+$_lang['ditsnews.settings.error'] = 'Chyba při ukládání nastavení';
